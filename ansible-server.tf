99ede158f9a455d1c4dfa8a78e18aa8a5b7b06a5
beaustar2 <beautypop4sure@gmail.com>
2024-01-21 06:16:39 -0700

    commit changes

--- ansible-server.tf
+++ ansible-server.tf
@@ -15,13 +15,13 @@ resource "aws_instance" "ansible-master" {
   provisioner "remote-exec" {
     inline = [
       "sudo yum -y install git",
-      "git clone https://github.com/beaustar2/Ansible-tf-project1.git",
-      "cd Ansible-tf-project1",
+      "git clone https://github.com/beaustar2/Ansible.git",
+      "cd Ansible",
       "sudo chmod 400 kiki.pem",
       "sudo yum -y install epel-release",
       "sudo yum -y install ansible",
-      "ansible -m ping -i host.ini n1",
-      "ansible-playbook setup_ecomm.yml -i host.ini",
+      "ansible -m ping -i host.ini node1",
+      "ansible-playbook setup-ecomm.yaml -i host.ini",
     ]
 
     connection {
